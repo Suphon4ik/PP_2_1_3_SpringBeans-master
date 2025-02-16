d4c6458c1af7b2545302badae156e4573d73f886
suphon4ik <supsuphonidinov03@gmail.com>
2025-02-16 19:06:06 +0300

    Initial commit

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+    public Dog() {
+    }
+    @Override
+    public String toString() {
+        return "Dog";
+    }
+}
